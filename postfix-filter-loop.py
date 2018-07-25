3f12899cc157f10b7bcecb940f29aa0d2873c361
Koray YILMAZ <kyilmaz80@gmail.com>
2018-07-25 21:42:59 +0300

    parent module import problem

--- postfix-filter-loop.py
+++ postfix-filter-loop.py
@@ -6,9 +6,16 @@ import smtpd
 import asyncore
 import smtplib
 import traceback
+import logging
+import os,sys,inspect
+
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir + '/emailrelay-dlp')
+
 import parse_msg
 import params
-
+from common import *
 # import parse_msg and params from
 # https://github.com/kyilmaz80/emailrelay-dlp postfix-python-filter branch
 
@@ -16,6 +23,14 @@ import params
 class CustomSMTPServer(smtpd.SMTPServer):
 
     def process_message(self, peer, mailfrom, rcpttos, data):
+        """
+        processes the message
+        :param peer: remote sender
+        :param mailfrom: mail from header
+        :param rcpttos: mail recipients
+        :param data: message object as string
+        :return:
+        """
         
         mailfrom.replace('\'', '')
         mailfrom.replace('\"', '')
@@ -23,14 +38,13 @@ class CustomSMTPServer(smtpd.SMTPServer):
         for recipient in rcpttos:
             recipient.replace('\'', '')
             recipient.replace('\"', '')
-        
-        print('Receiving message from:', peer)
-        print('Message addressed from:', mailfrom)
-        print('Message addressed to  :', rcpttos)
-        print('MSG >>')
-        print(data)
-        print('>> EOT')
-        
+
+        logging.info('Receiving message from: %s', peer)
+        logging.info('Message addressed from: %S', mailfrom)
+        logging.info('Message addressed to  %s:', rcpttos)
+        logging.info('MSG >>')
+        logging.info(data)
+        logging.info('>> EOT')
 
         try:
             # DO WHAT YOU WANNA DO WITH THE EMAIL HERE
@@ -40,18 +54,16 @@ class CustomSMTPServer(smtpd.SMTPServer):
             # Error handling is not really fantastic either.
             DLP_DETECTED = False
             # TODO: add timestamp to msg_file
-            # TODO: rewrite parse_msg to parse from string
-            msg_file = '/tmp/message'
-            with open(msg_file, 'w') as fp:
-                fp.write(data)
-            ret = parse_msg.main(msg_file)
+            # TODO: rewrite parse_msg to parse from string. DONE: in parse_msg.py 1.0.5 version
+            email_message = parse_msg.EmailMessage(message_string=data)
+            ret = email_message.search_leaks()
             if ret != 0:
                 DLP_DETECTED = True
             # pass
         except:
             pass
-            print('Something went south')
-            print(traceback.format_exc())
+            logging.error('Something went south')
+            logging.error(traceback.format_exc())
 
         try:
             if DLP_DETECTED == False:
@@ -60,45 +72,55 @@ class CustomSMTPServer(smtpd.SMTPServer):
                 server.quit()
                 # print('send successful')
             else:
-                print("DLP content found on message!")
+                logging.warning("DLP content found on message!")
+                logging.info("Archiving the message in " + params.FILTER_QUEUE_PATH)
+                fname = email_message.message_id
+                email_message.export_message(params.FILTER_QUEUE_PATH + '/' + clean_str(fname) + '.eml')
         except smtplib.SMTPException:
-            print('Exception SMTPException')
+            logging.error('Exception SMTPException')
             pass
         except smtplib.SMTPServerDisconnected:
-            print('Exception SMTPServerDisconnected')
+            logging.error('Exception SMTPServerDisconnected')
             pass
         except smtplib.SMTPResponseException:
-            print('Exception SMTPResponseException')
+            logging.error('Exception SMTPResponseException')
             pass        
         except smtplib.SMTPSenderRefused:
-            print('Exception SMTPSenderRefused')
+            logging.error('Exception SMTPSenderRefused')
             pass        
         except smtplib.SMTPRecipientsRefused:
-            print('Exception SMTPRecipientsRefused')
+            logging.error('Exception SMTPRecipientsRefused')
             pass        
         except smtplib.SMTPDataError:
-            print('Exception SMTPDataError')
+            logging.error('Exception SMTPDataError')
             pass        
         except smtplib.SMTPConnectError:
-            print('Exception SMTPConnectError')
+            logging.error('Exception SMTPConnectError')
             pass        
         except smtplib.SMTPHeloError:
-            print('Exception SMTPHeloError')
+            logging.error('Exception SMTPHeloError')
             pass        
         except smtplib.SMTPAuthenticationError:
-            print('Exception SMTPAuthenticationError')
+            logging.error('Exception SMTPAuthenticationError')
             pass
         except:
-            print('Undefined exception')
+            logging.error('Undefined exception')
             print(traceback.format_exc())
 
         return
 
 if __name__ == "__main__":
-    
+
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - \
+                                                %(threadName)s - %(message)s',
+                        filename='/tmp/postfix-filter.log',
+                        filemode='a', level=logging.DEBUG)
     FILTER_SERVER_IP = params.LHOST
     REMOTE_POSTFIX_IP = params.RHOST
     REMOTE_POSTFIX_PORT = params.RPORT
     FILTER_SERVER_PORT = params.LPORT
+
+    logging.info('------------------------------------------')
+    logging.info('Server is waiting for data on port: %s', FILTER_SERVER_PORT)
     server = CustomSMTPServer((FILTER_SERVER_IP, FILTER_SERVER_PORT), None)
     asyncore.loop()
